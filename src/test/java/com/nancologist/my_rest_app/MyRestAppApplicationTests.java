c0e77aa62130f2d570830f96d3a76e1e92d082f1
morteza <nancologist@gmail.com>
2025-05-01 12:54:25 +0200

    Init project

---
+++ src/test/java/com/nancologist/my_rest_app/MyRestAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nancologist.my_rest_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyRestAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
