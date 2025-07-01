6ce661c27ac6df5df2f6b89320674e85a1a39b23
morteza <nancologist@gmail.com>
2025-07-01 04:04:03 +0200

    Add image of colors to the database

---
+++ src/main/java/com/nancologist/importer/writer/ColorImageWriter.java
@@ -0,0 +1,46 @@
+package com.nancologist.importer.writer;
+
+import java.io.File;
+import java.sql.*;
+import java.util.Arrays;
+import java.util.Objects;
+
+public class ColorImageWriter {
+    private static final String JDBC_URL = "jdbc:postgresql://localhost:5432/springular?currentSchema=car360";
+    public static final String IMGS_PATH = "src/main/resources/data/color-images/";
+
+    public static void main(String[] args) throws SQLException {
+        try {
+            Connection connection = DriverManager.getConnection(JDBC_URL, "admin", "admin");
+            String updateQuery = "UPDATE colors SET image = lo_get(?::oid) WHERE code = ?";
+            String importQuery = "SELECT lo_import(?)";
+            try (PreparedStatement updateQueryPS = connection.prepareStatement(updateQuery);
+                 PreparedStatement importQueryPS = connection.prepareStatement(importQuery)
+            ) {
+                File imgDir = new File(IMGS_PATH);
+                for (File file: Objects.requireNonNull(imgDir.listFiles())) {
+                    if (file.isFile()) {
+                        String imagePath = file.getAbsolutePath();
+                        importQueryPS.setString(1, imagePath);
+
+                        String colorCode = Arrays.stream(file.getName().split("-")).map(item -> item.split("\\.")[0]).reduce((a, b) -> b).orElseThrow(() -> new IllegalStateException("Color ID not found"));
+
+                        ResultSet result = importQueryPS.executeQuery();
+                        if (result.next()) {
+                            int largeObjectId = result.getInt("lo_import");
+                            updateQueryPS.setInt(1, largeObjectId);
+                            updateQueryPS.setString(2, colorCode.toUpperCase());
+                            int updateCount = updateQueryPS.executeUpdate();
+                            System.out.println(updateCount + " rows have been updated!");
+                        }
+
+                        String unlinkQuery = "SELECT lo_unlink(?)"; // For clean up the database // Todo: implement this.
+                    }
+                }
+            }
+            connection.close();
+        } catch (SQLException e) {
+            throw new SQLException(e);
+        }
+    }
+}
