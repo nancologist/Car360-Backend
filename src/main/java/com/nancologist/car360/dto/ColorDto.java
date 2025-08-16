934b8b54b780f4e0d51d00d5cc4def6164d01bd6
morteza <nancologist@gmail.com>
2025-08-16 07:09:54 +0200

    Update response dtos

--- src/main/java/com/nancologist/car360/dto/ColorDto.java
+++ src/main/java/com/nancologist/car360/dto/ColorDto.java
@@ -1,13 +1,24 @@
 package com.nancologist.car360.dto;
 
 import com.nancologist.car360.model.Color;
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Getter;
+
+import static io.swagger.v3.oas.annotations.media.Schema.RequiredMode.REQUIRED;
 
 @Getter
 public class ColorDto {
+
+    @Schema(requiredMode = REQUIRED)
     private final Long id;
+
+    @Schema(requiredMode = REQUIRED)
     private final String code;
+
+    @Schema(requiredMode = REQUIRED)
     private final String name;
+
+    @Schema(requiredMode = REQUIRED)
     private final String manufacturer;
 
     public ColorDto(Color color) {
--- src/main/java/com/nancologist/car360/dto/auth/LoginResponse.java
+++ src/main/java/com/nancologist/car360/dto/auth/LoginResponse.java
@@ -1,13 +1,24 @@
 package com.nancologist.car360.dto.auth;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+
+import static io.swagger.v3.oas.annotations.media.Schema.RequiredMode.REQUIRED;
 
 @AllArgsConstructor
 @Getter
 public class LoginResponse {
+
+    @Schema(requiredMode = REQUIRED)
     private String token;
+
+    @Schema(requiredMode = REQUIRED)
     private Long userId;
+
+    @Schema(requiredMode = REQUIRED)
     private String username;
+
+    @Schema(requiredMode = REQUIRED)
     private String email;
 }
